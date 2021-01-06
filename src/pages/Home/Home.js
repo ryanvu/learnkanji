7f946e09234776b978d8c77ab219208a492b2e2f
Ryan Vu <ryanvu2003@gmail.com>
2021-01-06 02:42:28 -0500

    created select drop down to select different lists of kanji by grades

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,17 +3,60 @@ import { Link } from 'react-router-dom';
 import './Home.scss';
 
 const Home = () => {
+    const storedList = localStorage.getItem('myList')
+    const [list, setList] = useState(storedList ? storedList : 'grade-1')
+    const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [data, setData] = useState([])
+
+    const options = ["grade-1", "grade-2", "grade-3", "grade-4", "grade-5", "grade-6", "grade-8", "joyo", "jinmeiyo"]
+
+    const toggle = () => {
+        setShow(prev => !prev);
+    }
+
+    const handleList = (e) => {
+        e.preventDefault();
+        setLoading(true)
+        localStorage.setItem('myList', e.target.id)
+        setList(e.target.id)
+        toggle()
+        const fetchNewList =  async () => {
+            const res = await fetch(`https://kanjiapi.dev/v1/kanji/${e.target.id}`)
+            const data = await res.json()
+            setData(data); 
+            setLoading(false);
+        }
+        fetchNewList();
+    }
+
     useEffect(() => {
+        localStorage.setItem('myList', list)
         const fetchData = async () => {
-            const res = await fetch('https://kanjiapi.dev/v1/kanji/grade-2')
+            const res = await fetch(`https://kanjiapi.dev/v1/kanji/${list}`)
             const data = await res.json()
             setData(data);
         }
         fetchData();
-    },[])
+    },[list])
+
     return (
-        <div>
+        <div className="home">
+            <div className="home__select">
+                <div onClick={toggle} className="home__select-current">
+                    {list.charAt(0).toUpperCase() + list.slice(1, list.length).replace("-", " ")}
+                </div>
+                <div className="home__select-cont">
+                {show && options.map((op, i) => {
+                    return (
+                        <div onClick={handleList}key={i}>
+                            <p className="home__select-options"id={op}>{op.charAt(0).toUpperCase() + op.slice(1, op.length).replace("-", " ")}</p>
+                        </div>
+                    )
+                })}
+                </div>
+            </div>
+            {!loading && 
             <div className="home__container">
                 {data.map((k,i) => {
                     return (
@@ -25,6 +68,8 @@ const Home = () => {
                     )
                 })}
             </div>
+            }
+            
         </div>
     )
 }
--- src/pages/Kanji/Kanji.js
+++ src/pages/Kanji/Kanji.js
@@ -3,6 +3,7 @@ import './Kanji.scss';
 
 const Kanji = ({match}) => {
     const [kanji, setKanji] = useState([]);
+    const [words, setWords] = useState([]);
     const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
@@ -10,43 +11,76 @@ const Kanji = ({match}) => {
             const res = await fetch(`https://kanjiapi.dev/v1/kanji/${match.params.kanji}`)
             const data = await res.json();
             setKanji(data)
+            fetchWords();
+        }
+
+        const fetchWords = async () => {
+            const res = await fetch(`https://kanjiapi.dev/v1/words/${match.params.kanji}`)
+            const data = await res.json();
+            setWords(data)
             setLoading(false)
         }
-        fetchKanji()
-    },[])
+        fetchKanji();
+        
+    },[match.params.kanji])
 
+    const getRandomWords = (words) => {
+        const randomIndex = () => Math.floor(Math.random() * words.length)
+        return [words[randomIndex()],words[randomIndex()],words[randomIndex()],words[randomIndex()],words[randomIndex()]]
+    }
+    console.log(kanji)
+    const wordDisplay = getRandomWords(words);
+    console.log(wordDisplay)
 
     return (
         <div>
-            <p>{JSON.stringify(kanji)}</p>
         {!loading && 
-        <div className="card">
+        <>
+            <div className="card">
                 <p className="card__heisig">{kanji.heisig_en}</p>
-                <h1>{kanji.kanji}</h1>
+                <h1 className="card__kanji">{kanji.kanji}</h1>
                 <ul className="card__meanings">
-                    {kanji.meanings.map((m,i) => {
-                        return <li key={i}>{m}</li>
-                    })}
+                    {kanji.meanings.length > 1 ? kanji.meanings.map((m,i) => {
+                        return <li className="card__meanings-word"key={i}>{m}</li>
+                    }) : null}
                 </ul>
+                {kanji.kun_readings.length > 0 && <p>kun yomi</p>}
                 <ul className="card__kun">
                     {kanji.kun_readings.map((m,i) => {
                         return <li key={i}>{m}</li>
                     })}
                 </ul>
+                {kanji.on_readings.length > 0 && <p>on yomi</p>}
                 <ul className="card__on">
                     {kanji.on_readings.map((m,i) => {
                         return <li key={i}>{m}</li>
                     })}
                 </ul>
+                {kanji.name_readings.length > 0 && <p>nanori</p>}
                 <ul className="card__nanori">
                     {kanji.name_readings.map((m,i) => {
-                        return <li key={i}>{m}</li>
+                        return <li className="card__nanori-word"key={i}>{m}</li>
                     })}
                 </ul>
+                <p className="card_jlpt">JLPT: {kanji.jlpt}</p>
             </div>
+            <div className="words">
+            {wordDisplay.map((word, i) => {
+                return (
+                    <div key={i} className="words__container">
+                        <p className="words__word">{word.variants[0].written}</p>
+                        <div className="words__info">
+                            <p>{word.variants[0].pronounced}</p>
+                            <p>{word.meanings[0].glosses[0]}</p>
+                        </div> 
+                    </div>
+                )
+            })}
+
+        </div>
+        </>
             }
             
-
         </div>
     )
 }
